4caa7cc7562d5c702d7c3d88a7b5d64304baa92a
Graham Grubbs <ggrubbs9@gmail.com>
2021-02-01 15:32:37 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+const gitHubUrl =
+  "https://statsapi.web.nhl.com/api/v1/people/8478402/stats?stats=yearByYear";
 
 function App() {
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const response = await axios.get(gitHubUrl);
+    console.log(response.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
