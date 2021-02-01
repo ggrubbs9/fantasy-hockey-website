50c456f377d66e00e71a9ae76d50b623f53ac4c0
Graham Grubbs <ggrubbs9@gmail.com>
2021-02-01 16:25:24 -0600

    add bottom nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import RestoreIcon from "@material-ui/icons/Restore";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import HomeComponent from "./pages/Home";
 
 const gitHubUrl =
   "https://statsapi.web.nhl.com/api/v1/people/8478402/stats?stats=yearByYear";
 
+const useStyles = makeStyles({
+  root: {
+    width: 500,
+  },
+});
+
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  const classes = useStyles();
+
   useEffect(() => {
     getData();
   }, []);
@@ -14,10 +30,6 @@ function App() {
     const response = await axios.get(gitHubUrl);
     console.log(response.data);
   };
-
-  function Home() {
-    return <h2>Home</h2>;
-  }
 
   function About() {
     return <h2>About</h2>;
@@ -30,20 +42,6 @@ function App() {
   return (
     <Router>
       <div>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-          <li>
-            <Link to='/dashboard'>Dashboard</Link>
-          </li>
-        </ul>
-
-        <hr />
-
         {/*
           A <Switch> looks through all its children <Route>
           elements and renders the first one whose path
@@ -53,7 +51,7 @@ function App() {
         */}
         <Switch>
           <Route exact path='/'>
-            <Home />
+            <HomeComponent />
           </Route>
           <Route path='/about'>
             <About />
@@ -62,6 +60,34 @@ function App() {
             <Dashboard />
           </Route>
         </Switch>
+
+        <BottomNavigation
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          showLabels
+          className={classes.root}
+        >
+          <BottomNavigationAction
+            component={Link}
+            to='/'
+            label='Recents'
+            icon={<RestoreIcon />}
+          />
+          <BottomNavigationAction
+            component={Link}
+            to='/about'
+            label='Favorites'
+            icon={<FavoriteIcon />}
+          />
+          <BottomNavigationAction
+            component={Link}
+            to='/dashboard'
+            label='Nearby'
+            icon={<LocationOnIcon />}
+          />
+        </BottomNavigation>
       </div>
     </Router>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import { Container } from "@material-ui/core";
+
+function HomeComponent() {
+  return (
+    <Container maxWidth='sm'>
+      <h1>hi</h1>
+    </Container>
+  );
+}
+
+export default HomeComponent;
