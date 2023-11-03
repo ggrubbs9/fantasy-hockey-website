fb55d781362fa610553581dcb1b56fcf45f7dde9
ggrubbs1 <71671354+ggrubbs1@users.noreply.github.com>
2023-11-03 11:24:49 -0600

    another test

--- src/store/AllData/allData.actions.js
+++ src/store/AllData/allData.actions.js
@@ -46,6 +46,7 @@ export const teamPlayersLoaded = (players) => ({
   https://medium.com/mad-semicolon/fetch-initial-data-on-page-load-in-react-redux-application-16f4d8228543
 */
 
+
 export const fetchFantasyTeamPlayers = () => {
   return async dispatch => {
     try {
