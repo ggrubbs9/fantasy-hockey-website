7de999450a58ebe7666684af670880ec6ed654d5
ggrubbs1 <36514251+ggrubbs9@users.noreply.github.com>
2023-11-10 15:56:40 -0700

    try THIS

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-  databaseUrl: 'https://fantasy-hockey-website-default-rtdb.firebaseio.com',
+  databaseUrl: process.env.REACT_APP_DATABASE_URL,
 };
 console.log(firebaseConfig);
 
